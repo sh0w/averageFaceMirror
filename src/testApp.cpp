b74c4a7bd07368a2896e684bbb0e6b7c13842790
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2015-11-20 23:46:56 -0500

    added min face size

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -16,10 +16,12 @@ void testApp::setup() {
 		cam.initGrabber(WIDTH, HEIGHT);
     }
     tracker.setup();
+    tracker.setHaarMinSize(HEIGHT / 4);
     classifier.classify(tracker);
     
     numFaces = 0;
     
+    output.allocate(WIDTH, HEIGHT, GL_RGB);
     allFaces.allocate(WIDTH, HEIGHT, GL_RGB);
     currentFace.allocate(WIDTH, HEIGHT, GL_RGB);
     
@@ -31,7 +33,7 @@ void testApp::setup() {
     a = new unsigned long[WIDTH * HEIGHT * 3];
     
     
-    classifier.load("expressions");
+//    classifier.load("expressions");
 }
 
 
@@ -61,16 +63,21 @@ void testApp::setupPS3Eye() {
     vidGrabber.setDesiredFrameRate(camFrameRate);
     vidGrabber.initGrabber(camWidth, camHeight);
     
-    vidGrabber.setAutogain(true);
+    vidGrabber.setAutogain(false);
     vidGrabber.setAutoWhiteBalance(true);
+    vidGrabber.setBrightness(50);
+    vidGrabber.setSharpness(255);
+    vidGrabber.setContrast(50);
+    vidGrabber.setRedBalance(120);
+    vidGrabber.setBlueBalance(133);
+    
     
     tracker.setRescale(1);
-    
-    
 }
 
 void testApp::update() {
     
+    vidGrabber.setHue(100);
     if(usePS3) {
         vidGrabber.update();
         
@@ -95,6 +102,9 @@ void testApp::update() {
 }
 
 void testApp::draw() {
+    output.begin();
+    
+    
     ofPushMatrix();
     if(tracker.getFound()) {
         
@@ -123,17 +133,67 @@ void testApp::draw() {
         ofRotate(-asin((rightEyePos.y - leftEyePos.y)/
                        ofDist(leftEyePos.x, leftEyePos.y, rightEyePos.x,  rightEyePos.y)) * 180/PI);
         
+        /*
+        ofLogNotice("eye-dist");
+        ofLogNotice(ofToString(ofDist(leftEyePos.x, leftEyePos.y, rightEyePos.x,  rightEyePos.y)));
+        ofLogNotice("mouth-eye-dist");
+        ofLogNotice(ofToString(ofDist((leftEyePos.x + rightEyePos.x)/2, (leftEyePos.y + rightEyePos.y)/2, mouthPos.x, mouthPos.y)));
         
+        ofLogNotice("righteye x, y");
+        ofLogNotice(ofToString(rightEyePos.x));
+        ofLogNotice(ofToString(rightEyePos.y));
+        ofLogNotice("lefteye x, y");
+        ofLogNotice(ofToString(leftEyePos.x));
+        ofLogNotice(ofToString(leftEyePos.y));
+        
+        ofLogNotice("between left and right");
+        ofLogNotice(ofToString(leftEyePos.x + ));
+        ofLogNotice(ofToString(leftEyePos.y));
+        
+        ofLogNotice("mouth x, y");
+        ofLogNotice(ofToString(mouthPos.x));
+        ofLogNotice(ofToString(mouthPos.y));*/
+        
+        //ofPushMatrix();
+        
+        // scale everything down to make left + right eye always at same distance
         ofScale(270 / ofDist(leftEyePos.x, leftEyePos.y, rightEyePos.x,  rightEyePos.y),
                 270 / ofDist(leftEyePos.x, leftEyePos.y, rightEyePos.x,  rightEyePos.y));
         
-        ofTranslate(-leftEyePos);
+        
+        
+        /*float scalefactorForMouth = 136 / ofDist(leftEyePos.x, leftEyePos.y, mouthPos.x,  mouthPos.y);
+        
+        ofLogNotice("scalefactorForMouth:");
+        ofLogNotice(ofToString(scalefactorForMouth));
+        
+        ofScale(1, scalefactorForMouth);
+         */
+        
+        //ofPopMatrix();
+         
+         
+        ofTranslate(-leftEyePos.x, -leftEyePos.y);
+         
+         
+        
+        //ofTranslate(-leftEyePos);
+        
+        // scale everything down to mouth at same position too!
+        //ofLogNotice("i d like to scale y:");
+        //ofLogNotice(ofToString(323 / ofDist((leftEyePos.x + rightEyePos.x)/2, (leftEyePos.y + rightEyePos.y)/2, mouthPos.x, mouthPos.y)));
+        //ofScale(1, 323 / ofDist((leftEyePos.x + rightEyePos.x)/2, (leftEyePos.y + rightEyePos.y)/2, mouthPos.x, mouthPos.y));
+        
+        
+        // scale everything down to mouth at same position too!
+        //ofScale(1, 323 / ofDist((leftEyePos.x + rightEyePos.x)/2, (leftEyePos.y + rightEyePos.y)/2, mouthPos.x, mouthPos.y));
         
         if(usePS3) {
             videoTexture.draw(0,0);
         } else {
             cam.draw(0, 0);
         }
+        
         ofPopMatrix();
         
         if(ofGetElapsedTimeMillis() > timestampLastFaceSaved + 1000) {
@@ -165,8 +225,16 @@ void testApp::draw() {
         } else {
             cam.draw(0,0,ofGetWidth()/2, ofGetHeight()/2);
         }
-        tracker.draw();
+        //tracker.draw(0,0,ofGetWidth()/2, ofGetHeight()/2);
         
+        /*
+        ofPushStyle();
+        ofSetColor(0,255,0);
+        ofEllipse(tracker.getImageFeature(ofxFaceTracker::INNER_MOUTH).getCentroid2D(), 10,10);
+        ofSetColor(255,0,0);
+        ofEllipse(tracker.getImageFeature(ofxFaceTracker::OUTER_MOUTH).getCentroid2D(), 10,10);
+        ofPopStyle();
+         */
         ofDrawBitmapString(ofToString((int) ofGetFrameRate()), 10, 20);
         ofDrawBitmapString(ofToString(numFaces), 10, 35);
         ofDrawBitmapString(ofToString((int)ofGetElapsedTimeMillis()/1000), 10, 50);
@@ -181,6 +249,32 @@ void testApp::draw() {
     if(classifier.getProbability(5) < 0.9 && justDidExpression) {
         justDidExpression = false;
     }
+
+    
+    output.end();
+    
+    ofBackground(0);
+    ofSetBackgroundColor(0);
+    
+    ofPushMatrix();
+    
+    ofTranslate(WIDTH/2, HEIGHT/2);
+    //ofRotateX(-mouseX/5);
+    //ofLogNotice(ofToString(-mouseY/5));
+    ofRotateY(-mouseX/5);
+    
+    ofTranslate(-WIDTH/2, -HEIGHT/2);
+    
+    ofTranslate(100, 0, -100);
+    
+    output.draw(0,0);
+    
+    ofDrawBitmapString(ofToString(numFaces), 10, 35);
+    ofPopMatrix();
+    
+    ofDrawBitmapString(ofToString(numFaces), 10, 35);
+    //output.draw(0,0);
+    
 }
 
 void testApp::addCurrentFaceToAllFaces() {
